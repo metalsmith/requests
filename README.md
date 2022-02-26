0bb3d797b2dd8d5dbbef0d9ab322375b9e9e0716
Kevin Van Lierde <kevin.van.lierde@gmail.com>
2022-02-26 01:03:49 +0100

    Initial implementation

--- README.md
+++ README.md
@@ -1,17 +1,6 @@
-_Note: This is a template repository_
+# @metalsmith/requests
 
-Usage:
-
-1. Click "Use this template", see also https://docs.github.com/en/repositories/creating-and-managing-repositories/creating-a-repository-from-a-template, fill in new plugin details
-2. Search and replace all `core-plugin` `CorePlugin` and `corePlugin` matches with the name of the plugin
-3. Change description in package.json & match in repo description
-4. Remove this text
-
----
-
-# @metalsmith/~core-plugin~
-
-A metalsmith plugin to...
+A metalsmith plugin to query API's and add the results to files and metadata
 
 [![metalsmith: core plugin][metalsmith-badge]][metalsmith-url]
 [![npm: version][npm-badge]][npm-url]
@@ -28,35 +17,69 @@ An optional features section (if there are many), or an extended description of 
 NPM:
 
 ```
-npm install @metalsmith/~core-plugin~
+npm install @metalsmith/requests
 ```
 
 Yarn:
 
 ```
-yarn add @metalsmith/~core-plugin~
+yarn add @metalsmith/requests
 ```
 
 ## Usage
 
-Pass `@metalsmith/~core-plugin~` to `metalsmith.use` :
+Pass `@metalsmith/requests` to `metalsmith.use` :
 
 ```js
-const ~corePlugin~ = require('@metalsmith/~core-plugin~')
+const requests = require('@metalsmith/requests')
 
-metalsmith.use(~corePlugin~()) // defaults
-metalsmith.use(~corePlugin~({  // explicit defaults
-  ...
-}))
+metalsmith.use(requests()) // defaults
+
+metalsmith.use(
+  requests({
+    // single GET request set
+    url: 'https://www.google.com/humans.txt',
+    out: { metadata: 'google.humans' }
+  })
+)
+
+metalsmith.use(
+  requests({
+    // parameterized w options
+    url: 'https://api.github.com/repos/:owner/:repo/contents/README.md',
+    params: [
+      { owner: 'metalsmith', repo: 'drafts' },
+      { owner: 'metalsmith', repo: 'sass' }
+    ],
+    out: { path: 'core-plugins/:owner-:repo.md' },
+    options: {
+      method: 'GET',
+      auth: `metalsmith:${process.env.GITHUB_TOKEN}`,
+      headers: {
+        Accept: 'application/vnd.github.3.raw'
+      }
+    }
+  })
+)
 ```
 
 ### Options
 
-Optional section with list or table of options, if the plugin has a lot of options
+You can pass a single requests config, or an array of request configs to `@metalsmith/requests`. Every request config has the following options:
 
-### Specific usage example
+| Property  | Type       | Description                                                                                                                                                                                                                                        |
+| :-------- | :--------- | :------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- | ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- | --- |
+| `url`     | `string`   | A url or url pattern with [params placeholders](#params-placeholders). Supported protocols are `http:`,`https:`, and `file:`.                                                                                                                      |
+| `params`  | `Object[]` | An object with params to fill placeholders in the url pattern.                                                                                                                                                                                     |
+| `out`     | `{path     | metadata:string}`                                                                                                                                                                                                                                  | An object of the form `{ metadata: 'key.path.target' }` to store the response in metadata, or an object of the form `{ path: 'path/to/file.ext' }` to store the response in a file in the `build` |     |
+| `out`     | `Function` | If you need more flexibility, you can specify a callback instead, which is passed a result object with the data (response body) and request config, and the metalsmith files and instance: `out: ({ data, config }, files, metalsmith) => { ... }` |
+| `options` | `Object`   | An object with options you would pass to [Node's https.request](). If `method` is not set, it will default to `GET`. `@metalsmith/requests` also adds a `User-Agent: @metalsmith/requests` header if no other headers are set.                     |
 
-Document a first specific usage example, the title can be "Achieve x by doing y"
+You can also pass a string (`.use(['https://<url>'])`) as shorthand, which will expand to `{ url: '<url>', options: { method: 'GET', headers: { 'User-Agent': '@metalsmith/requests' }}}`.
+
+### Params placeholders
+
+`@metalsmith/requests` uses [regexparam]() to parse urls.
 
 ### Specific usage example
 
@@ -64,51 +87,47 @@ Document a second specific usage example, the title can be "Achieve x by doing y
 
 ### Debug
 
-To enable debug logs, set the `DEBUG` environment variable to `@metalsmith/~core-plugin~`:
+To enable debug logs, set the `DEBUG` environment variable to `@metalsmith/requests`:
 
 Linux/Mac:
 
 ```
-DEBUG=@metalsmith/~core-plugin~
+DEBUG=@metalsmith/requests
 ```
 
 Windows:
 
 ```
-set "DEBUG=@metalsmith/~core-plugin~"
+set "DEBUG=@metalsmith/requests"
 ```
 
 Alternatively you can set `DEBUG` to `@metalsmith/*` to debug all Metalsmith core plugins.
 
 ### CLI usage
 
-To use this plugin with the Metalsmith CLI, add `@metalsmith/~core-plugin~` to the `plugins` key in your `metalsmith.json` file:
+To use this plugin with the Metalsmith CLI, add `@metalsmith/requests` to the `plugins` key in your `metalsmith.json` file:
 
 ```json
 {
   "plugins": [
     {
-      "@metalsmith/~core-plugin~": {}
+      "@metalsmith/requests": {}
     }
   ]
 }
 ```
 
-## Credits (optional)
-
-Special thanks to ... for ...
-
 ## License
 
 [MIT](LICENSE)
 
-[npm-badge]: https://img.shields.io/npm/v/@metalsmith/~core-plugin~.svg
-[npm-url]: https://www.npmjs.com/package/@metalsmith/~core-plugin~
-[ci-badge]: https://github.com/metalsmith/~core-plugin~/actions/workflows/test.yml/badge.svg
-[ci-url]: https://github.com/metalsmith/~core-plugin~/actions/workflows/test.yml
+[npm-badge]: https://img.shields.io/npm/v/@metalsmith/requests.svg
+[npm-url]: https://www.npmjs.com/package/@metalsmith/requests
+[ci-badge]: https://github.com/metalsmith/requests/actions/workflows/test.yml/badge.svg
+[ci-url]: https://github.com/metalsmith/requests/actions/workflows/test.yml
 [metalsmith-badge]: https://img.shields.io/badge/metalsmith-core_plugin-green.svg?longCache=true
 [metalsmith-url]: https://metalsmith.io
-[codecov-badge]: https://img.shields.io/coveralls/github/metalsmith/~core-plugin~
-[codecov-url]: https://coveralls.io/github/metalsmith/~core-plugin~
-[license-badge]: https://img.shields.io/github/license/metalsmith/~core-plugin~
+[codecov-badge]: https://img.shields.io/coveralls/github/metalsmith/requests
+[codecov-url]: https://coveralls.io/github/metalsmith/requests
+[license-badge]: https://img.shields.io/github/license/metalsmith/requests
 [license-url]: LICENSE
--- test/index.js
+++ test/index.js
@@ -9,22 +9,248 @@ function fixture(p) {
   return require('path').resolve(__dirname, 'fixtures', p)
 }
 
-describe('@metalsmith/~core-plugin~', function () {
+const googleHumansTxt =
+  "Google is built by a large team of engineers, designers, researchers, robots, and others in many different sites across the globe. It is updated continuously, and built with more tools and technologies than we can shake a stick at. If you'd like to help us out, see careers.google.com.\n"
+
+describe('@metalsmith/requests', function () {
   it('should export a named plugin function matching package.json name', function () {
     const namechars = name.split('/')[1]
     const camelCased = namechars.split('').reduce((str, char, i) => {
       str += namechars[i - 1] === '-' ? char.toUpperCase() : char === '-' ? '' : char
       return str
     }, '')
-    assert.strictEqual(plugin().name, camelCased.replace(/~/g, ''))
+    assert.strictEqual(plugin().name, camelCased)
   })
+
   it('should not crash the metalsmith build when using default options', function (done) {
     Metalsmith(fixture('default'))
       .use(plugin())
       .build((err) => {
-        assert.strictEqual(err, null)
+        if (err) done(err)
         equals(fixture('default/build'), fixture('default/expected'))
         done()
       })
+  })
+
+  it("should add a response's content to metadata", function (done) {
+    const ms = Metalsmith(fixture('default'))
+    ms.use(
+      plugin({
+        url: 'https://www.google.com/humans.txt',
+        out: { metadata: 'googlehumans' }
+      })
+    ).process((err) => {
+      if (err) done(err)
+      assert.strictEqual(ms.metadata().googlehumans, googleHumansTxt)
+      done()
+    })
+  })
+
+  it("should add a response's content to a file, appending its data, and preserving its other metadata", function (done) {
+    const ms = Metalsmith(fixture('default'))
+    ms.use(function (files) {
+      files['test.md'] = {
+        layout: 'default.njk',
+        contents: Buffer.from('testest')
+      }
+    })
+      .use(
+        plugin({
+          url: 'https://www.google.com/humans.txt',
+          out: { path: 'test.md' }
+        })
+      )
+      .process((err, files) => {
+        if (err) done(err)
+        assert.strictEqual(files['test.md'].contents.toString(), 'testest' + googleHumansTxt)
+        done()
+      })
+  })
+
+  it('should treat files with a "request" key in metadata as entries', function (done) {
+    Metalsmith(fixture('implicit-entries'))
+      .use(plugin())
+      .build((err) => {
+        if (err) return done(err)
+        equals(fixture('implicit-entries/build'), fixture('implicit-entries/expected'))
+        done()
+      })
+  })
+
+  it('should allow specifying a single request config', function (done) {
+    Metalsmith(fixture('single-request-option'))
+      .use(
+        plugin({
+          url: 'https://webketje.com/assets/css/main.css',
+          out: { path: 'main.css' }
+        })
+      )
+      .build((err) => {
+        if (err) done(err)
+        equals(fixture('single-request-option/build'), fixture('single-request-option/expected'))
+        done()
+      })
+  })
+
+  it('should parse a response as JSON when "out.json" is set', function (done) {
+    const ms = Metalsmith(fixture('default'))
+    ms.use(
+      plugin({
+        // parameterized w options
+        url: 'https://api.github.com/repos/metalsmith/drafts/contents/README.md',
+        out: { metadata: 'readme', json: true },
+        options: {
+          method: 'GET',
+          auth: `metalsmith:${process.env.GITHUB_TOKEN}`,
+          headers: {
+            Accept: 'application/json'
+          }
+        }
+      })
+    ).process((err) => {
+      if (err) done(err)
+      assert.strictEqual(
+        ms.metadata().readme.download_url,
+        'https://raw.githubusercontent.com/metalsmith/drafts/master/README.md'
+      )
+      done()
+    })
+  })
+
+  it('should provide basic support for GraphQL queries', function (done) {
+    if (!process.env.GITHUB_TOKEN) {
+      this.skip()
+    }
+    const gqlQuery = `query {
+        repository(owner: "metalsmith", name: "sass") {
+          object(expression: "master:README.md") {
+            ... on Blob { text }
+          }
+        }
+      }`
+    const ms = Metalsmith(fixture('default'))
+    ms.use(
+      plugin({
+        url: 'https://api.github.com/graphql',
+        out: { metadata: 'sassreadme' },
+        body: JSON.stringify({ query: gqlQuery }),
+        options: {
+          method: 'POST',
+          headers: {
+            Authorization: 'bearer ' + process.env.GITHUB_TOKEN,
+            'Content-Type': 'application/json',
+            Accept: 'application/json'
+          }
+        }
+      })
+    ).process((err) => {
+      if (err) done(err)
+      assert.strictEqual(
+        ms.metadata().sassreadme.slice(0, 82),
+        '{"data":{"repository":{"object":{"text":"# @metalsmith/sass\\n\\nA Metalsmith plugin'
+      )
+      done()
+    })
+  })
+
+  it('should replace path placeholders in out and url options accordingly', function (done) {
+    Metalsmith(fixture('placeholders'))
+      .use(
+        plugin({
+          url: 'https://webketje.com/assets/css/:filename.css',
+          out: { path: ':filename.css' },
+          params: [{ filename: 'main' }, { filename: 'style' }]
+        })
+      )
+      .build((err) => {
+        if (err) done(err)
+        equals(fixture('placeholders/build'), fixture('placeholders/expected'))
+        done()
+      })
+  })
+
+  describe('should throw error', function () {
+    it('"unsupported_protocol" when the protocol is not supported', function (done) {
+      Metalsmith(fixture('default'))
+        .use(
+          plugin({
+            url: 'ftp://bad-protocol.com',
+            out: { metadata: 'bad' }
+          })
+        )
+        .process((err) => {
+          assert.strictEqual(err.code, 'unsupported_protocol')
+          done()
+        })
+    })
+
+    it('"invalid_http_method" when the HTTP method is invalid', function (done) {
+      Metalsmith(fixture('default'))
+        .use(
+          plugin({
+            url: 'http://bad-httpmethod.com',
+            out: { metadata: 'bad' },
+            options: {
+              method: 'FAULTY'
+            }
+          })
+        )
+        .process((err) => {
+          assert.strictEqual(err.code, 'invalid_http_method')
+          done()
+        })
+    })
+
+    it('"invalid_outconfig" when the outconfig is invalid', function (done) {
+      Metalsmith(fixture('default'))
+        .use(
+          plugin({
+            url: 'http://bad-outoption.com',
+            out: { faulty: 'bad' }
+          })
+        )
+        .process((err) => {
+          assert.strictEqual(err.code, 'invalid_outconfig')
+          done()
+        })
+    })
+
+    it('"http_error" when the HTTP method is invalid', function (done) {
+      if (!process.env.GITHUB_TOKEN) {
+        this.skip()
+      }
+      Metalsmith(fixture('default'))
+        .use(
+          plugin({
+            url: 'https://graphql.github.com/graphql',
+            out: { metadata: 'bad' },
+            body: JSON.stringify({ query: 'query test { view { user { login }}}' }),
+            options: {
+              headers: {
+                Authorization: 'bearer ' + process.env.GITHUB_TOKEN,
+                'Content-Type': 'application/json',
+                Accept: 'application/json'
+              }
+            }
+          })
+        )
+        .process((err) => {
+          assert.strictEqual(err.code, 'http_error')
+          done()
+        })
+    })
+
+    it('"invalid_json" when "out.json" is set and the response is invalid', function (done) {
+      const ms = Metalsmith(fixture('default'))
+      ms.use(
+        plugin({
+          url: 'https://www.google.com/humans.txt',
+          out: { metadata: 'googlehumans', json: true }
+        })
+      ).process((err) => {
+        assert.strictEqual(err.code, 'invalid_json')
+        done()
+      })
+    })
   })
 })
